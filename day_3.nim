4565bf5dc7772dd2e2dcee4bd6a4589323197239
IAvecilla <iavecilla@fi.uba.ar>
2023-02-27 18:31:10 -0300

    Refactor for day 3 solutions

--- day_3.nim
+++ day_3.nim
@@ -32,10 +32,9 @@ proc solve_second_part() =
         total_priority: int
 
     while file.read_line(line):
+        elves_group.add(line)
         if elves_group.len() != 3:
-            elves_group.add(line)
             continue
-        echo elves_group
         let
             first_elf = elves_group[0]
             second_elf = elves_group[1]
@@ -46,8 +45,6 @@ proc solve_second_part() =
 
         if group_badge.len() == 0:
             elves_group.delete(0..2)
-            elves_group.add(line)
-            echo elves_group
             continue
 
         if group_badge[0].isUpperAscii():
@@ -55,23 +52,7 @@ proc solve_second_part() =
         else:
             inc total_priority, ord(group_badge[0]) - ord('a') + 1
 
-        echo "a"
         elves_group.delete(0..2)
-        elves_group.add(line)
-        echo elves_group
-
-    let
-        first_elf = elves_group[0]
-        second_elf = elves_group[1]
-        third_elf = elves_group[2]
-
-    let group_badge = first_elf.filterIt(second_elf.contains(it) and
-            third_elf.contains(it))
-
-    if group_badge[0].isUpperAscii():
-        inc total_priority, ord(group_badge[0]) - ord('A') + 27
-    else:
-        inc total_priority, ord(group_badge[0]) - ord('a') + 1
 
     echo "The answer for the first part is: ", total_priority
 
